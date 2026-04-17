b11b2c6dcffe87d1f28ceeab28ed433fe9ea382e
Amr Al-odini <160176809+43k6@users.noreply.github.com>
2026-04-17 03:10:36 +0300

    Add Parametters Folder and class for Response and request prams

---
+++ FinGuardAI.DataAccess/Parameters/RequestFilterParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinGuardAI.DataAccess.Parameters
+{
+    public class RequestFilterParameters
+    {
+
+        public string? Status { get; set; }
+        public string? RequestCategory { get; set; }
+        public int? CreatedBy { get; set; }
+        public decimal? MinAmount { get; set; }
+        public decimal? MaxAmount { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+    }
+}
---
+++ FinGuardAI.DataAccess/Parameters/ResponseFilterParameters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinGuardAI.DataAccess.Parameters
+{
+    public class ResponseFilterParameters
+    {
+        public string? Decision { get; set; }
+        public int? RequestId { get; set; }
+        public int? CreatedBy { get; set; } // هذا هو الـ UserId الذي اتخذ القرار
+        public decimal? MinAcceptedAmount { get; set; }
+        public decimal? MaxAcceptedAmount { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
